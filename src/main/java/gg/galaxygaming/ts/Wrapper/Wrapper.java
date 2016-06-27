d83841ba5f53c83955bb5b974f608e5f0ed847f7
pupnewfster <richard@freimer.com>
2016-06-27 08:19:12 -0400

    Use client Library instead of server library
    startConnection gives errors still is why it is commented out

--- src/main/java/gg/galaxygaming/ts/Wrapper/Wrapper.java
+++ src/main/java/gg/galaxygaming/ts/Wrapper/Wrapper.java
@@ -1,8 +1,8 @@
 package gg.galaxygaming.ts.Wrapper;
 
 import com.sun.jna.*;
-import com.sun.jna.ptr.*;
-import gg.galaxygaming.ts.JanetTS;
+import com.sun.jna.ptr.LongByReference;
+import com.sun.jna.ptr.PointerByReference;
 
 public class Wrapper {
 
@@ -17,36 +17,44 @@ public class Wrapper {
     private static LongByReference CONNECTION_ID;
 
     public interface TSLibrary extends Library {
-        //TSLibrary INSTANCE = (TSLibrary)Native.loadLibrary((Platform.isWindows() ? Platform.is64Bit() ? "lib/ts3client_win64.dll" : "lib/libts3client_linux_x86.so" :
-                //Platform.is64Bit() ? "lib/libts3client_linux_amd64.so" : "lib/ts3client_win32.dll"), TSLibrary.class);
-        TSLibrary INSTANCE = (TSLibrary)Native.loadLibrary((Platform.isWindows() ? (Platform.is64Bit() ? "bin/ts3server_win64.dll" : "bin/ts3server_win32.dll") :
-                (Platform.is64Bit() ? "bin/libts3server_linux_amd64.so" : "bin/libts3server_linux_x86.so")), TSLibrary.class);
+        TSLibrary INSTANCE = (TSLibrary) Native.loadLibrary((Platform.isWindows() ? Platform.is64Bit() ? "lib/ts3client_win64.dll" : "lib/libts3client_linux_x86.so" :
+                Platform.is64Bit() ? "lib/libts3client_linux_amd64.so" : "lib/ts3client_win32.dll"), TSLibrary.class);
+        //TSLibrary INSTANCE = (TSLibrary)Native.loadLibrary((Platform.isWindows() ? (Platform.is64Bit() ? "bin/ts3server_win64.dll" : "bin/ts3server_win32.dll") :
+                //(Platform.is64Bit() ? "bin/libts3server_linux_amd64.so" : "bin/libts3server_linux_x86.so")), TSLibrary.class);
 
-        int ts3server_initServerLib(PointerByReference functionPointers, int usedLogTypes, String logFileFolder);
-
-        int ts3server_destroyServerLib();
-
-        int ts3server_freeMemory(Pointer pointer);
+        int ts3client_initClientLib(PointerByReference functionPointers, PointerByReference functionRarePointers, int usedLogTypes, String logFileFolder, String resourcesFolder);
+        int ts3client_spawnNewServerConnectionHandler(int port, LongByReference result);
+        int ts3client_createIdentity(PointerByReference result);
+        int ts3client_startConnection(long serverConnectionHandlerID, String identity, String ip, int port, String nickname, StringArray defaultChannelArray, String defaultChannelPassword, String serverPassword);
+        int ts3client_getClientLibVersion(PointerByReference result);
+        int ts3client_stopConnection(long serverConnectionHandlerID, String quitMessage);
+        int ts3client_destroyServerConnectionHandler(long serverConnectionHandlerID);
+        int ts3client_destroyClientLib();
+        int ts3client_freeMemory(Pointer pointer);
+        //int ts3client_requestSendChannelTextMsg(PointerByReference serverConnectionHandlerID, String message, String returnCode);
     }
 
     public static void main(String[] args) {
         try {
             PointerByReference p = new PointerByReference();
-            int error = TSLibrary.INSTANCE.ts3server_initServerLib(p, 0x0002, null);
+            int error = TSLibrary.INSTANCE.ts3client_initClientLib(p, null, 0x0002, null, "lib/");
             System.out.println(error);
-            /*CONNECTION_ID = new LongByReference();
+            CONNECTION_ID = new LongByReference();
             error = TSLibrary.INSTANCE.ts3client_spawnNewServerConnectionHandler(0, CONNECTION_ID);
             System.out.println(error);
             System.out.println(CONNECTION_ID.getValue());
 
-
             PointerByReference name = new PointerByReference();
             error = TSLibrary.INSTANCE.ts3client_createIdentity(name);
             System.out.println(error);
-            error = TSLibrary.INSTANCE.ts3client_startConnection(CONNECTION_ID, name.getValue().getString(0), JanetTS.getInstance().getConfig().getString("tsHost"), 9987, "JanetC", null, "", "");
-            System.out.println(error);
-            PointerByReference rcode = new PointerByReference();
-            TSLibrary.INSTANCE.ts3client_requestSendChannelTextMsg(CONNECTION_ID, "test", 0, rcode);*/
+            //PointerByReference version = new PointerByReference();
+            //TSLibrary.INSTANCE.ts3client_getClientLibVersion(version);
+            //System.out.println(version.getValue().getString(0));
+
+            //error = TSLibrary.INSTANCE.ts3client_startConnection(CONNECTION_ID.getValue(), name.getValue().getString(0), JanetTS.getInstance().getConfig().getString("tsHost"), 9987, "Janet", null, "", "");
+            //System.out.println(error);
+            //PointerByReference rcode = new PointerByReference();
+            //TSLibrary.INSTANCE.ts3client_requestSendChannelTextMsg(CONNECTION_ID, "test", 0, rcode);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -54,7 +62,9 @@ public class Wrapper {
 
     public static void exit() {
         try {
-            TSLibrary.INSTANCE.ts3server_destroyServerLib();
+            TSLibrary.INSTANCE.ts3client_stopConnection(CONNECTION_ID.getValue(), "Disconnected.");
+            TSLibrary.INSTANCE.ts3client_destroyServerConnectionHandler(CONNECTION_ID.getValue());
+            TSLibrary.INSTANCE.ts3client_destroyClientLib();
         } catch (Exception e) {
             e.printStackTrace();
         }
